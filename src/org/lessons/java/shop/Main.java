6ce9fcda7d8661e3074e0e48f50da466d48a5e94
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-05 15:56:26 +0200

    ended ex 1 senza bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,5 +1,30 @@
 package org.lessons.java.shop;
 
 public class Main {
+    public static void main(String[] args) {
+//        passo nel nuovo prodotto istanziato i parametri che avevo nel costruttore (nome,descrizione,prezzo,iva)
+        Prodotto prodotto1 = new Prodotto("Air-Jordan-1","Scarpa iconica degli anni 90'",350,22);
 
+        System.out.println("Codice: " + prodotto1.getCodice());
+        System.out.println("Nome: " + prodotto1.getNome());
+        System.out.println("Descrizione: " + prodotto1.getDescrizione());
+        System.out.println("Prezzo: " + prodotto1.getPrezzo());
+        System.out.println("Aliquota IVA: " + prodotto1.getIva() + "%");
+
+        System.out.println("Prezzo ivato: " + prodotto1.prezzoIvato());
+        System.out.println("Nome completo: " + prodotto1.NomeCompleto());
+
+//        provo ad istanziare un nuovo prodotto e stampo
+
+        Prodotto prodotto2 = new Prodotto("Air-Max","Scarpa iconica degli ultimi 15 anni",180,22);
+
+        System.out.println("Codice: " + prodotto2.getCodice());
+        System.out.println("Nome: " + prodotto2.getNome());
+        System.out.println("Descrizione: " + prodotto2.getDescrizione());
+        System.out.println("Prezzo: " + prodotto2.getPrezzo());
+        System.out.println("Aliquota IVA: " + prodotto2.getIva() + "%");
+
+        System.out.println("Prezzo ivato: " + prodotto2.prezzoIvato());
+        System.out.println("Nome completo: " + prodotto2.NomeCompleto());
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,19 +1,73 @@
 package org.lessons.java.shop;
-
+import java.util.Random;
 public class Prodotto {
     // Attributi
-        private int codice;
-        String nome;
-        String descrizione;
-        double  prezzo;
-        int iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double  prezzo;
+    private int iva;
+
    //Costruttore
-    public Prodotto(int codice,String nome,String descrizione,double  prezzo, int iva){
-        this.codice = codice;
-
-
-
-    //Getters e Setters
+    public Prodotto(String nome,String descrizione,double  prezzo, int iva) {
+        this.codice = new Random().nextInt(0, 1000);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
 
     }
+//    I getter non hanno parametri e
+//    restituiscono un tipo di dato = al tipo
+//    dell'attributo mentre
+//    I setter restituiscono void e hanno un
+//    parametro di tipo = al tipo dell'attributo
+
+    //Getters e Setters
+        public int getCodice() {
+            return codice;
+        }
+        public String getNome() {
+            return nome;
+        }
+        public String getDescrizione() {
+            return descrizione;
+        }
+        public double getPrezzo() {
+            return prezzo;
+        }
+        public int getIva() {
+            return iva;
+        }
+
+    // non inserisco il setter del codice in quando ha accesso solo per la lettura,per gli altri creo un setter public che dara' void(
+//     traducibile come "vuoto" dichiarare un metodo void significa dire che il metodo non ritornerà alcun valore ed in tal caso la keyword return può essere anche omessa.
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public void setDescrizione(String descrizione) {
+            this.descrizione = descrizione;
+        }
+        public void setPrezzo(double prezzo) {
+            this.prezzo = prezzo;
+        }
+        public void setIva(int iva) {
+            this.iva = iva;
+        }
+
+//        alcuni setter inseriti ma non utilizzati
+
+//    Metodi
+
+//    Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+        public double prezzoIvato(){
+            return prezzo + prezzo * iva / 100;
+        }
+    //    Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+        public String NomeCompleto() {
+
+            return codice + "-" + nome;
+        }
+
+
 }
